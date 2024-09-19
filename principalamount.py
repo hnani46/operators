0b0cec7d1c49a680fe4a7caa1fdd8709f2be8d96
hnani46 <22hr1a3246@mtieat.org>
2024-09-19 13:38:33 +0530

    Create  principalamount.py

---
+++ principalamount.py
@@ -0,0 +1,21 @@
+...
+You are given principal amount, rate of interest per annum, and time in years. You need to calculate the simple interest.
+...
+Sample Input 1
+1000
+5
+2
+Sample Output 1
+100.0
+Sample Input 2
+5000
+8.5
+3
+Sample Output 2
+1275.0
+Sample Input 3
+5000
+8.5
+3
+Sample Output 3
+525.0
