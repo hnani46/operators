2f666a1a0edc6bffd68ecdc63fc108bb2b81fa5a
hnani46 <22hr1a3246@mtieat.org>
2024-09-19 13:47:37 +0530

    Update distance.py

--- distance.py
+++ distance.py
@@ -12,3 +12,15 @@ Sample Input
 Sample Output
 The distance between Ajay's house and Chandru's house is 5.00
 
+Solution
+import math
+
+x1 = int(input())
+y1 = int(input())
+x2 = int(input())
+y2 = int(input())
+
+distance = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
+print(f"The distance between Ajay's house and Chandru's house is {distance:.2f}")
+
