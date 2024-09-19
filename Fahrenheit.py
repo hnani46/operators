d9f04cd787c1043b70c810a94bfc9a844ba8a5ae
hnani46 <22hr1a3246@mtieat.org>
2024-09-19 13:40:15 +0530

    Create Fahrenheit.py

---
+++ Fahrenheit.py
@@ -0,0 +1,15 @@
+...
+You are given a temperature in Celsius. You need to convert it to Fahrenheit.
+...
+Sample Input 1
+0
+Sample Output 1
+32.0
+Sample Input 2
+25
+Sample Output 2
+77.0
+Sample Input 3
+100
+Sample Output 3
+212.0
