1d7e03d18f5a9d2b46279c7369c74ead0ff21cc9
hnani46 <22hr1a3246@mtieat.org>
2024-09-19 12:36:32 +0530

    Create  BCCI .py

---
+++ BCCI .py
@@ -0,0 +1,15 @@
+...
+MS Dhoni is a grade A player according to BCCI Central Contracts in 2016. MSD's net worth in 2016 is around 31 million and is said to be the richest Indian cricketer.
+Apart from his salary as an Indian cricketer, Dhoni endorses various popular brands and earns a large amount from endorsements. Besides that individual and team bonuses are also given on the basis of individual and team performances. So precisely the sources of income for Dhoni are from - Salary, Bonuses and Awards through Winning and Endorsements.
+Write a program that finds Dhoni's percentage of income earned from each of the 3 individual sources.
+...
+Sample Input and Output 1:
+100
+20
+80
+50.00 10.00 40.00
+Sample Input and Output2:
+50000
+10000
+35000
+52.63 10.53 36.84
